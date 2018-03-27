b7bb38bc148ec21ac019bfb8580471e7862ccd85
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-03-27 01:57:54 -0600

    Formats code, creates separate method for 'instant' speed

--- src/needle/main/Main.java
+++ src/needle/main/Main.java
@@ -16,21 +16,21 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 public class Main {
-	
+
 	protected static int sleepTime;
 	protected static Thread t;
 	protected static boolean running;
 
 	public static boolean isNum(String str) {
-	    int size = str.length();
-	    for (int i = 0; i < size; i++) {
-	        if (!Character.isDigit(str.charAt(i))) {
-	            return false;
-	        }
-	    }
-	    return size > 0;
+		int size = str.length();
+		for (int i = 0; i < size; i++) {
+			if (!Character.isDigit(str.charAt(i))) {
+				return false;
+			}
+		}
+		return size > 0;
 	}
-	
+
 	public static boolean tossNeedle(NeedlesComponent comp) {
 		double locationX = Math.random();
 		double locationY = Math.random();
@@ -48,28 +48,32 @@ public class Main {
 			return true;
 		return false;
 	}
-	
-	public static void simulate(String input, JLabel status, JButton sim, JButton clr, JButton can, NeedlesComponent comp) {
+
+	public static void simulate(String input, JLabel status, NeedlesComponent comp, JButton sim, JButton clr,
+			JButton can) {
 		t = new Thread() {
 			@Override
-		    public void run() {
+			public void run() {
 				comp.clearNeedles();
 				if (isNum(input)) {
 					long needles = Long.valueOf(input);
 					double matches = 0;
 					for (long i = 1; i <= needles; i++) {
-						if (!running) break;
+						if (!running || sleepTime == 0) {
+							comp.clearNeedles();
+							simulateInstant(input, status, comp, sim, clr, can);
+							break;
+						}
 						if (tossNeedle(comp)) {
 							matches++;
 						}
 						comp.repaint();
 						try {
-							if (sleepTime > 0)
-								Thread.sleep(sleepTime);
+							Thread.sleep(sleepTime);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
-						status.setText("Pi = " + (needles / matches) * 2);
+						status.setText("Pi = " + (i / matches) * 2);
 						status.repaint();
 					}
 				} else {
@@ -82,7 +86,28 @@ public class Main {
 		};
 		t.start();
 	}
-	
+
+	public static void simulateInstant(String input, JLabel status, NeedlesComponent comp, JButton sim, JButton clr,
+			JButton can) {
+		if (isNum(input)) {
+			comp.clearNeedles();
+			long needles = Long.valueOf(input);
+			double matches = 0;
+			for (long i = 1; i <= needles; i++) {
+				if (tossNeedle(comp)) {
+					matches++;
+				}
+			}
+			status.setText("Pi = " + (needles / matches) * 2);
+		} else {
+			status.setText("<html><font color=red>Please input a whole number</font></html>");
+		}
+		comp.repaint();
+		sim.setEnabled(true);
+		clr.setEnabled(true);
+		can.setEnabled(false);
+	}
+
 	public static void createAndShowGUI() {
 		sleepTime = 200;
 		running = true;
@@ -90,22 +115,22 @@ public class Main {
 		JFrame frame = new JFrame("Buffon's Needle");
 		JPanel board = new JPanel(new BorderLayout());
 		JPanel input = new JPanel(new GridLayout(3, 2));
-		JLabel status = new JLabel("Input a number, then click simulate:", JLabel.CENTER);	
-		JTextField in = new JTextField(JTextField.CENTER);		
+		JLabel status = new JLabel("Input a number, then click simulate:", JLabel.CENTER);
+		JTextField in = new JTextField(JTextField.CENTER);
 		JButton sim = new JButton("Simulate");
 		JButton clr = new JButton("Clear");
 		JButton can = new JButton("Cancel");
-		String[] speedOptions = {"Normal", "Fast", "Instant"};
+		String[] speedOptions = { "Normal", "Fast", "Instant" };
 		JComboBox<String> spd = new JComboBox<String>(speedOptions);
-		
-		//Board JPanel
+
+		// Board JPanel
 		board.add(comp, BorderLayout.CENTER);
 		board.setPreferredSize(new Dimension(500, 500));
-		
-		//Input JPanel
+
+		// Input JPanel
 		in.setHorizontalAlignment(SwingConstants.CENTER);
 		can.setEnabled(false);
-		
+
 		input.add(status);
 		input.add(in);
 		input.add(clr);
@@ -113,21 +138,20 @@ public class Main {
 		input.add(can);
 		input.add(spd);
 		input.setPreferredSize(new Dimension(500, 103));
-		
-		
-		//Add components
+
+		// Add components
 		frame.add(board, BorderLayout.CENTER);
 		frame.add(input, BorderLayout.PAGE_END);
-		
-		//JFrame settings
+
+		// JFrame settings
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
-		/*Button listeners*/
-		//Clear button
+
+		/* Button listeners */
+		// Clear button
 		clr.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -136,8 +160,8 @@ public class Main {
 				comp.clearNeedles();
 			}
 		});
-		
-		//Simulate button
+
+		// Simulate button
 		sim.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -146,15 +170,22 @@ public class Main {
 				sim.setEnabled(false);
 				clr.setEnabled(false);
 				can.setEnabled(true);
-				simulate(input, status, sim, clr, can, comp);
+				if (sleepTime > 0) {
+					simulate(input, status, comp, sim, clr, can);
+				} else {
+					simulateInstant(input, status, comp, sim, clr, can);
+				}
 			}
 		});
-		
-		//Cancel button
+
+		// Cancel button
 		can.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				running = false;
+				sim.setEnabled(true);
+				clr.setEnabled(true);
+				can.setEnabled(false);
 				comp.clearNeedles();
 				try {
 					t.join();
@@ -165,29 +196,32 @@ public class Main {
 				status.repaint();
 			}
 		});
-		
-		//Speed selection
+
+		// Speed selection
 		spd.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JComboBox<String> cb = (JComboBox<String>) e.getSource();
-				switch((String)cb.getSelectedItem()) {
-					case "Normal": sleepTime = 200;
-								   break;
-					case "Fast": sleepTime = 100;
-								 break;
-					case "Instant": sleepTime = 0;
-									break;
+				switch ((String) cb.getSelectedItem()) {
+				case "Normal":
+					sleepTime = 200;
+					break;
+				case "Fast":
+					sleepTime = 100;
+					break;
+				case "Instant":
+					sleepTime = 0;
+					break;
 				}
 			}
 		});
 	}
-	
+
 	public static void main(String args[]) {
 		SwingUtilities.invokeLater(new Runnable() {
-		    public void run() {
-		        createAndShowGUI();
-		    }
+			public void run() {
+				createAndShowGUI();
+			}
 		});
 	}
 }
--- src/needle/main/NeedlesComponent.java
+++ src/needle/main/NeedlesComponent.java
@@ -17,53 +17,53 @@ public class NeedlesComponent extends JComponent {
 
 	private static class Needle {
 		final double x1;
-	    final double y1;
-	    final double x2;
-	    final double y2;
-	    final Color c;
+		final double y1;
+		final double x2;
+		final double y2;
+		final Color c;
 
-	    public Needle(double x1, double y1, double x2, double y2, Color c) {
-	        this.x1 = x1;
-	        this.y1 = y1;
-	        this.x2 = x2;
-	        this.y2 = y2;
-	        this.c = c;
-	    }   
+		public Needle(double x1, double y1, double x2, double y2, Color c) {
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+			this.c = c;
+		}
 	}
-	
+
 	private final LinkedList<Needle> needles = new LinkedList<Needle>();
 
 	public void addNeedle(double x1, double y1, double x2, double y2) {
 		Random rand = new Random();
 		float r = rand.nextFloat();
 		float g = rand.nextFloat();
-		float b = rand.nextFloat();;
+		float b = rand.nextFloat();
+		;
 		Color randomColor = new Color(r, g, b);
-	    needles.add(new Needle(x1, y1, x2, y2, randomColor));        
+		needles.add(new Needle(x1, y1, x2, y2, randomColor));
 	}
-	
+
 	public void clearNeedles() {
-	    needles.clear();
-	    repaint();
+		needles.clear();
+		repaint();
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics gr) {
-	    super.paintComponent(gr);
-	    Graphics2D g2 = (Graphics2D) gr;
-	    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                            RenderingHints.VALUE_ANTIALIAS_ON);
-	    g2.setColor(Color.BLACK);
-	    g2.drawLine(100, 0, 100, 500);
-	    g2.drawLine(200, 0, 200, 500);
-	    g2.drawLine(300, 0, 300, 500);
-	    g2.drawLine(400, 0, 400, 500);
-	    for (int i = 0; i < needles.size(); i++) {
-	    	Needle needle = needles.get(i);
-	    	Shape n = new Line2D.Double(needle.x1, needle.y1, needle.x2, needle.y2);
-	        g2.setColor(needle.c);
-	        g2.draw(n);
-	    }
+		super.paintComponent(gr);
+		Graphics2D g2 = (Graphics2D) gr;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setColor(Color.BLACK);
+		g2.drawLine(100, 0, 100, 500);
+		g2.drawLine(200, 0, 200, 500);
+		g2.drawLine(300, 0, 300, 500);
+		g2.drawLine(400, 0, 400, 500);
+		for (int i = 0; i < needles.size(); i++) {
+			Needle needle = needles.get(i);
+			Shape n = new Line2D.Double(needle.x1, needle.y1, needle.x2, needle.y2);
+			g2.setColor(needle.c);
+			g2.draw(n);
+		}
 	}
-	    
+
 }
